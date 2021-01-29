748770e66653c2b215e410c6fc56a5bf8172f776
Hala ALtahleh <halaaltahleh80@gmail.com>
2021-01-29 21:06:53 +0200

    commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var flowertype = prompt("what type the flower do you want?");
 var noflower = prompt("what many the flower do you want?");
+var hala = prompt("hala");
 
 var orderdetail;
-if (noflower >= 1 && noflower < 6) {
-    ordertdetail = "congrates you gained a 25% discount";
+
+if (noflower >= 1 && noflower <= 6) {
+    orderdetail = "congrates you gained a 25% discount";
 }
 else if (noflower > 6 && noflower < 10) {
     orderdetail = "congrates you gained a 50% discount";
@@ -15,6 +17,26 @@ else {
     orderdetail = "sorry your input error";
 }
 
-document.write(orderdetails)
+document.write(orderdetail)
 alert("thank you");
 confirm("are you happy in this discount?");
+
+
+var AGE = prompt("whats your age?");
+
+
+var result;
+if (AGE = 24) {
+    result = "thank you hala";
+}
+else
+    if (AGE >= 45) {
+        result = "my age 24 years";
+    }
+    else {
+        result = "goodby";
+    }
+
+document.write(result)
+alert("thank you")
+confirm("hallosh")
