d098dead332debaa96d6104f53a6da5587b847de
Hala ALtahleh <halaaltahleh80@gmail.com>
2021-01-29 22:41:19 +0200

    commit

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-var flowertype = prompt("what type the flower do you want?");
-var noflower = prompt("what many the flower do you want?");
-var hala = prompt("hala");
+var username = prompt("whats your name?");
+var age = prompt("how old are you?");
+var nocups = prompt("what many of acups do you want?");
 
-var orderdetail;
-
-if (noflower >= 1 && noflower <= 6) {
-    orderdetail = "congrates you gained a 25% discount";
+var result = "";
+if (username == "hala") {
+    result = "my name is hala";
 }
-else if (noflower > 6 && noflower < 10) {
-    orderdetail = "congrates you gained a 50% discount";
-}
-else if (noflower > 10) {
-    orderdetail = "congrates you gained a 50% and two flowers free";
+else if (username == "haya") {
+    result = "my name is haya";
 }
 else {
-    orderdetail = "sorry your input error";
+    result = "my name is hannn";
+}
+var equal = "";
+if (age == 24) {
+    equal = "im 24 years";
+}
+else {
+    equal = "error";
+}
+var order = "";
+if (nocups > 4 && nocups < 6) {
+    order = "this order between 4 and 6";
+}
+else {
+    order = "error";
 }
 
-document.write(orderdetail)
+
+document.write(result);
+document.write(equal);
+document.write(order);
 alert("thank you");
-confirm("are you happy in this discount?");
-
-
-var AGE = prompt("whats your age?");
-
-
-var result;
-if (AGE = 24) {
-    result = "thank you hala";
-}
-else if (AGE >= 45) {
-    result = "my age 24 years";
-}
-else {
-    result = "goodby";
-}
-
-
-document.write(result)
-alert("thank you")
-confirm("hallosh")
+confirm("are you interested with me?");
